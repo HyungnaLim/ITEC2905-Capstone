c8c3a8194ccbc8b699e8f607033688b8e5e5bec6
Your Name <you@example.com>
2024-09-04 19:32:03 -0500

    finished in class

--- Lab2/lab2_part1_author.py
+++ Lab2/lab2_part1_author.py
@@ -20,14 +20,19 @@ class Author:
         # print an error message if the book given has the same name as a book currently in the books list.
         # Do not add the duplicate book. (In other words, make sure the Author object's book list doesn't already contain that name).
 
+        # create a empty list to collect lowercase book titles from the self.book list
         books_in_lowercase = []
-        for book in self.books:
+        for book in self.books:     # collect lowercase titles by using for loop
             books_in_lowercase.append(book.lower())
 
+        # if the same title is already in the list, print error message. otherwise, add the title to the list.
         if title.lower() in books_in_lowercase:
             print('This book is already in the published book list.')
         else:
             self.books.append(title)
+
+
+# testing
 
 author1 = Author('Sally Rooney')
 
