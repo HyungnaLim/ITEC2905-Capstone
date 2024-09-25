80403082a9e64e8598a95039ff3ee41031d3d9ed
Your Name <you@example.com>
2024-09-24 22:16:34 -0500

    peewee practice

---
+++ Lab5-Database/cats.py
@@ -0,0 +1,92 @@
+from peewee import *
+
+db = SqliteDatabase('cats.sqlite')
+
+# define each field so that peewee can create entities to store data in a table
+# peewee will also create auto incrementing integer for id
+class Cat(Model):
+    name = CharField()
+    color = CharField()
+    age = IntegerField()
+
+    class Meta:
+        database = db
+
+    # string method to display information of the object
+    def __str__(self):
+        return f'{self.id}: {self.name}, {self.color}, {self.age}'
+
+db.connect()
+db.create_tables( [Cat] )   # square bracket here!
+
+# pragma TABLE_INFO(cat) - sqlite command to inspect the table info
+
+Cat.delete().execute()  # clear the database table
+
+# adding Cat data to the database
+zoe = Cat(name='Zoe', color='Ginger', age=3)
+zoe.save()  # make sure to save!
+
+holly = Cat(name='Holly', color='Tabby', age=5)
+holly.save()
+
+fluffy = Cat(name='Fluffy', color='Black', age=1)
+fluffy.save()
+
+cats = Cat.select()     # query object, can't access each cat by index
+for cat in cats:
+    print(cat)
+
+list_of_cats = list(cats)   # regular Python list, can access each object with index
+
+# update data
+fluffy.age = 2
+fluffy.save()
+
+print('After Fluffy update')
+cats = Cat.select()
+for cat in cats:
+    print(cat)
+
+# update with where clause & return the number of rows modified
+rows_modified = Cat.update(age=6).where(Cat.name == 'Holly').execute()
+
+print('After Holly update')
+cats = Cat.select()
+for cat in cats:
+    print(cat)
+
+print(f'{rows_modified} row updated!')
+
+buzz = Cat(name='Buzz', color='Gray', age=3)
+buzz.save()
+
+cats_who_are_3 = Cat.select().where(Cat.age == 3)
+for cat in cats_who_are_3:
+    print(cat, ' is three')
+
+cats_with_l_in_name = Cat.select().where(Cat.name % '*l*')   # star sign is a wild card
+for cat in cats_with_l_in_name:
+    print(cat, ' has l in name')
+
+# case-insensitive search using .contains()
+cats_with_b_in_name = Cat.select().where(Cat.name.contains('b'))
+for cat in cats_with_b_in_name:
+    print(cat, ' has b in name')
+
+zoe_from_db = Cat.get_or_none(name='Zoe')   # find 1 data from database
+print(zoe_from_db)
+
+zoe_from_db = Cat.get_or_none(name='Shadow')
+print(zoe_from_db)
+
+cat_1 = Cat.get_by_id(1)    # search by id
+print(cat_1)
+
+# This will raise error because the id doesn't exist
+# cat_100 = Cat.get_by_id(100)
+# print(cat_100)
+
+# use this method instead so that it returns none when the matching data does not exist
+cat_100 = Cat.get_or_none(Cat.id == 100)
+print(cat_100)
