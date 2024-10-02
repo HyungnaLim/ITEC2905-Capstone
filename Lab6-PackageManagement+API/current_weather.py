e758250968acf7711d9af56e17313181b6ec6cb2
Hyungna <hyungnalim@gmail.com>
2024-10-02 08:30:56 -0500

    practice API, environment variable, query parameters

--- Lab6-PackageManagement+API/current_weather.py
+++ Lab6-PackageManagement+API/current_weather.py
@@ -1,26 +1,35 @@
+from itertools import count
+
 import requests
 import os
 
 key = os.environ.get('WEATHER_KEY')
+url = f'https://api.openweathermap.org/data/2.5/weather'
 
-print(key)
+# query = {'q' : 'Tokyo,JP', 'units' : 'metric', 'appid' : key}
+#
+# data = requests.get(url, params=query).json()
+# print(data)
+# temp = data['main']['temp']
+#
+# print(f'Current temperature in {query.get('q')} is {temp}C')
+#
+#
+# # try another city
+# query2 = {'q' : 'Paris,FR', 'units' : 'metric', 'appid' : key}
+# data = requests.get(url, params=query2).json()
+# temp = data['main']['temp']
+# print(f'Current temperature in {query2.get('q')} is {temp}C')
 
-city = 'Minneapolis,US'
 
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}'
+# try with user input
+city = input('Enter the city: ').strip().upper()
+country = input('Enter the 2-letter country code: ').strip().upper()
 
-data = requests.get(url).json()
+q = f'{city},{country}'
+query_with_user_input = {'q':q, 'units':'metric', 'appid':key}
 
-print(data)
-
+data = requests.get(url, params=query_with_user_input).json()
 temp = data['main']['temp']
 
-print(f'Current temperature in {city} is {temp}F')
-
-
-# try another city
-city = 'Paris,FR'
-data = requests.get(url).json()
-temp = data['main']['temp']
-print(f'Current temperature in {city} is {temp}F')
-
+print(f'Current temperature in {q} is {temp}C')
