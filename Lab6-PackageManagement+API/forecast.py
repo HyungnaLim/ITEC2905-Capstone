37374db41b5a09abc378babe23d5afe66894616a
Hyungna <hyungnalim@gmail.com>
2024-10-02 21:25:16 -0500

    Lab part1 WIP

--- Lab6-PackageManagement+API/forecast.py
+++ Lab6-PackageManagement+API/forecast.py
@@ -1,58 +1,74 @@
 """
-Part 1: Weather Forecast
-Use the forecast API to create a detailed, neatly formatted 5-day forecast, for anywhere the user chooses. Ask the user for the location.
-Make sure your API key is not coded into your program. Your program should read the key from an environment variable.
-Use a query parameter dictionary in the request.
-Your forecast should show the temperature and unit (your choice of F or C), weather description, and wind speed for every three hour interval, over the next 5 days.
-Your program should handle errors. What type of errors do you anticipate? How will you deal with them?
+Weather Forecast
 
-Part 2: Time choice
-Will you show the local time in Minnesota, or the UTC time? Why? Add some comments to your program explaining your choice. Reading: Unix Time
-
-Part 3: Logging vs Print
-When do you use one, when do you use the other? Replace any message that are only of interest to the developer with logging. When you run your program, it should only print user-friendly messages.
-Make sure you can find the log output from your program.
-Should you log sensitive information, for example, values of API keys?
-
-To Submit: create GitHub repository for this program, with example log output. Submit repository link to the D2L dropbox.
 """
 
 import requests, os
 from datetime import datetime
 
-key = os.environ.get('WEATHER_KEY')
-url = 'https://api.openweathermap.org/data/2.5/forecast'
 
-city = input('Enter the city: ').upper().strip()
-country = input('Enter the 2-letter country code: ').upper().strip()
-q = f'{city},{country}'
-units = input('Choose units - Enter "m" for metric, "i" for imperial: ').lower()
+def get_api_key():
+    try:
+        key = os.environ.get('WEATHER_KEY')
+        return key
+    except Exception as e:
+        print('Cannot find API key; ', e)
 
-if units == 'm':
-    units = 'metric'
-    unit_temperature = 'C'
-    unit_speed = 'meter/sec'
-elif units == 'i':
-    units = 'imperial'
-    unit_temperature = 'F'
-    unit_speed = 'miles/hour'
-else:
-    units = 'standard'
-    unit_temperature = 'Kelvin'
-    unit_speed = 'meter/sec'
 
-query = {'q':q, 'units':units, 'appid':key}
+def get_user_input():
+    city = get_valid_input('Enter the city: ')
+    country = get_valid_input('Enter the 2-letter country code: ')
+    q = f'{city},{country}'
+    return q
 
-data = requests.get(url, params=query).json()
-# print(data)
 
-list_of_forecasts = data['list']
+# TODO: work on validations
+def get_valid_input(msg):
+    user_input = input(msg)
+    return user_input.strip().upper()
 
-print(f'\nWeather Forcast - {q}')
-for forecast in list_of_forecasts:
-    timestamp = forecast['dt']
-    date = datetime.fromtimestamp(timestamp)
-    temp = forecast['main']['temp']
-    description = forecast['weather'][0]['description']
-    wind_speed = forecast['wind']['speed']
-    print(f'{date} | Weather: {description}, Temperature: {temp} {unit_temperature}, Wind Speed: {wind_speed} {unit_speed}')
+
+def set_units():
+    units_choice = get_valid_input('Choose units - Enter "M" for metric, "I" for imperial: ')
+    if units_choice == 'M':
+        return 'metric', 'C', 'meter/sec'
+    elif units_choice == 'I':
+        return 'imperial', 'F', 'miles/hour'
+    else:
+        return 'standard', 'Kelvin', 'meter/sec'
+
+
+def get_data_from_api(query):
+    url = 'https://api.openweathermap.org/data/2.5/forecast'
+    json_data = requests.get(url, params=query).json()
+    return json_data
+
+
+def print_forecast(data, q, units_tuple):
+    list_of_forecasts = data['list']
+
+    print(f'\nWeather Forcast - {q}')
+    for forecast in list_of_forecasts:
+        timestamp = forecast['dt']
+        date = datetime.fromtimestamp(timestamp)
+        temp = forecast['main']['temp']
+        description = forecast['weather'][0]['description']
+        wind_speed = forecast['wind']['speed']
+        print(f'{date} | Weather: {description}, Temperature: {temp} {units_tuple[1]}, Wind Speed: {wind_speed} {units_tuple[2]}')
+
+
+def main():
+    try:
+        key = get_api_key()
+        city_and_country = get_user_input()
+        units_tuple = set_units()
+        query = {'q':city_and_country, 'units':units_tuple[0], 'appid':key}
+        forecast_data = get_data_from_api(query)
+        print_forecast(forecast_data, city_and_country, units_tuple)
+
+    except Exception as e:
+        print('Error:', e)
+
+
+if __name__ == "__main__":
+    main()
